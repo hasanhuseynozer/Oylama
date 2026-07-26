5bebb31f3013fd291404135c72357afd087273c4
hasanhuseynozer <95506504+hasanhuseynozer@users.noreply.github.com>
2026-07-26 14:16:59 +0300

    Delete test directory

--- test/env.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Env } from "../src/lib/types";
-
-// Type the bindings available via `env` from "cloudflare:test".
-declare module "cloudflare:test" {
-	interface ProvidedEnv extends Env {}
-}
