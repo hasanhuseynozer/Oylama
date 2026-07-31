c7e4614f939fa8ae66c11e39e9852e82d1604853
hasanhuseynozer <hasanhuseynozer@gmail.com>
2026-07-31 11:07:30 +0300

    Remove network panel edge halos

--- public/home-layout-v5.js
+++ public/home-layout-v5.js
@@ -13,7 +13,7 @@
   const grid=document.getElementById('serverGrid');
   const portal=document.getElementById('portalView');
   const frame=document.getElementById('portalFrame');
-  const requestedStyleHref='/requested-ui-fixes-v6.css?v=20260731-4';
+  const requestedStyleHref='/requested-ui-fixes-v6.css?v=20260731-5';
   const menuCubeStyleHref='/header-menu-cube-v1.css?v=20260731-0930';
   const initialFrameHeight=680;
 
